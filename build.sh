06c5395c43cb1b11a9b56c1af3c08c4692767932
Daniel Baldwin <danielcbaldwin@gmail.com>
2015-10-30 11:22:43 -0600

    reducing build oss and archs as well as renaming the resulting files

--- build.sh
+++ build.sh
@@ -2,21 +2,13 @@
 
 build() {
   echo "Building for Linux..."
-  GOOS=linux GOARCH=amd64 go build -o ./bin/linux/amd64/docker-cron .
-  GOOS=linux GOARCH=386 go build -o ./bin/linux/386/docker-cron .
-  GOOS=linux GOARCH=arm go build -o ./bin/linux/arm/docker-cron .
+  GOOS=linux GOARCH=amd64 go build -o ./bin/docker-cron .
+  GOOS=linux GOARCH=386 go build -o ./bin/docker-cron.linux.386 .
+  GOOS=linux GOARCH=arm go build -o ./bin/docker-cron.linux.arm .
 
   echo "Building for Mac..."
-  GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/amd64/docker-cron .
-  GOOS=darwin GOARCH=386 go build -o ./bin/darwin/386/docker-cron .
-
-  echo "Building for FreeBSD..."
-  GOOS=freebsd GOARCH=amd64 go build -o ./bin/freebsd/amd64/docker-cron .
-  GOOS=freebsd GOARCH=386 go build -o ./bin/freebsd/386/docker-cron .
-
-  echo "Building for Windows..."
-  GOOS=windows GOARCH=amd64 go build -o ./bin/windows/amd64/docker-cron .
-  GOOS=windows GOARCH=386 go build -o ./bin/windows/386/docker-cron .
+  GOOS=darwin GOARCH=amd64 go build -o ./bin/docker-cron.darwin.amd64 .
+  GOOS=darwin GOARCH=386 go build -o ./bin/docker-cron.darwin.386 .
 }
 
 build
